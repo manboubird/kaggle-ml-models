46d2f7479cbfb4efede8f41a3023db2de8772cfa
Toshiaki Toyama <ttoyama@knownstylenolife.com>
2017-07-20 18:21:05 +0900

    fix script

--- zillow/bin/lookup_property.bash
+++ zillow/bin/lookup_property.bash
@@ -2,7 +2,16 @@
 
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 INPUT_DIR="${SCRIPT_DIR}/../input"
+
 PROPERTY_CSV="properties_2016.csv"
+TRAIN_CSV="train_2016_v2.csv"
 
-head -n 100 "${INPUT_DIR}/${PROPERTY_CSV}" | csvlook | less -S
+LINE_NUM=100
 
+output=""
+
+for f in "${PROPERTY_CSV}" "${TRAIN_CSV}";do
+  output="$(printf "${output}\n\n ${f}\n")"
+  output="$(printf "${output}\n $(head -n "${LINE_NUM}" "${INPUT_DIR}/${f}" | csvlook -z 100)")"
+done
+echo "${output}" # | less -S
